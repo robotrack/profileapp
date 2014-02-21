309336e0ce082e2510b304b4676c55245d6766e7
Stephen Gonthier <stephen.gonthier@gmail.com>
2014-02-20 21:45:23 -0800

    test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require tinymce
 //= require bootstrap.min 
-//= require turbolinks
+//= require jquery.cleditor.min
 //= require_tree .
+$("#section_content").cleditor();
